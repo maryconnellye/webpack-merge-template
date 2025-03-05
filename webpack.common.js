f9c182be5d65ffe72e788de26cf95d6588ab6cb4
Mary Connelly <maryconnellye@gmail.com>
2025-03-05 11:01:50 -0600

    Added eslint and prettier

--- webpack.common.js
+++ webpack.common.js
@@ -5,37 +5,37 @@ module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-	filename: "main.js",
-	path: path.resolve(__dirname, "dist"),
-	clean: true,
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   devtool: "source-map",
   devServer: {
-	watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/template.html"],
   },
   plugins: [
-	new HtmlWebpackPlugin({
-	  template: "./src/template.html",
-	}),
-  ],  
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
-	rules: [
-	  {
-		test: /\.css$/i,
-		use: ["style-loader", "css-loader"],
-	  },
-	  {
-		test: /\.html$/i,
-		loader: "html-loader",
-	  },
-	  {
-		test: /\.(png|svg|jpg|jpeg|gif)$/i,
-		type: "asset/resource",
-	  },
-	  {
-		test: /\.(woff|woff2|eot|ttf|otf)$/i,
-		type: 'asset/resource',
-	  },
-	],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
